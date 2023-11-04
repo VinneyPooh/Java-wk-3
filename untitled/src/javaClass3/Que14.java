de7323ec7cf2d5db9f4a369dfe4bd738c91ab423
VinneyPooh <soni.zarana@gmail.com>
2023-11-04 02:15:01 +0000

    hw

--- untitled/src/javaClass3/Que14.java
+++ untitled/src/javaClass3/Que14.java
@@ -1,4 +1,5 @@
 package javaClass3;
+
 // Write a java program to print the numbers between 1 to 100 which can be divided by 3 and 5 separately.
 public class Que14 {
     public static void main(String[] args) {
@@ -9,20 +10,18 @@ public class Que14 {
     public static void m1() {
         int i;
         for (i = 1; i <= 100; i++) {
-
-            if (i % 3 == 0){
-                System.out.print(" " + i );
-
+            if (i % 3 == 0) {
+                System.out.print(" "+ i);
             }
         }
+        System.out.println();
     }
+
     public static void m2() {
         int i;
         for (i = 1; i <= 100; i++) {
-
-            if (i % 5 == 0){
-                System.out.print("\n "  + i);
-
+            if (i % 5 == 0) {
+                System.out.print(" "+i);
             }
         }
     }
