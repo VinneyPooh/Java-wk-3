ce00055de3493f8c4e576b6311d7f294c8f944d5
VinneyPooh <soni.zarana@gmail.com>
2023-11-04 01:57:23 +0000

    hw

--- untitled/src/javaClass3/Que1.java
+++ untitled/src/javaClass3/Que1.java
@@ -6,7 +6,8 @@ import java.util.Scanner;
 public class Que1 {
 
     public static void main(String[] args) { // main method
-        m1();
+       m1();
+       m2();
 
     }
     // static method
@@ -14,10 +15,18 @@ public class Que1 {
         Scanner sc = new Scanner(System.in); // create a scanner object
         System.out.println("Enter a number - ");
         int num = sc.nextInt(); // using variable which take the input as scanner
-        if (num%2 == 0){ // even odd  logic condition
+        if (num % 2 == 0) { // even odd  logic condition
             System.out.println(" the number is Even");
         } else
             System.out.println(" the number is Odd");
+    }
+    public  static void m2(){
+        Scanner sc = new Scanner(System.in); // create a scanner object
+        System.out.println("Enter a number - ");
+        int num = sc.nextInt(); // using variable which take the input as scanner
 
+        System.out.println("Enter a number - ");
+        String result = (num % 2 == 0)?  " Even number. "  :  "Odd number.  ";  // use tenary operator
+        System.out.println(result); // print output.
     }
 }
